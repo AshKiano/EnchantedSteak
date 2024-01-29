7dd4fe773772b71ca943a3f6952a1153a3242c9a
jvyskocil <ash.kiano@gmail.com>
2024-01-29 12:01:34 +0100

    version 1.4

--- src/main/java/com/ashkiano/enchantedsteak/EnchantedSteak.java
+++ src/main/java/com/ashkiano/enchantedsteak/EnchantedSteak.java
@@ -27,6 +27,9 @@ public class EnchantedSteak extends JavaPlugin implements Listener {
         setupCraftingRecipe();
 
         Metrics metrics = new Metrics(this, 19479);
+
+        this.getLogger().info("Thank you for using the EnchantedSteak plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+
     }
 
     private void setupCraftingRecipe() {
