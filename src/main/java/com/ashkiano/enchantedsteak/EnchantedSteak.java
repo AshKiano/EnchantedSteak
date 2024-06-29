887ae4ced1631b500129c6236af61682a78d98eb
jvyskocil <ash.kiano@gmail.com>
2024-06-29 09:18:25 +0200

    version 1.5

--- src/main/java/com/ashkiano/enchantedsteak/EnchantedSteak.java
+++ src/main/java/com/ashkiano/enchantedsteak/EnchantedSteak.java
@@ -14,8 +14,14 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.json.JSONObject;
 
 public class EnchantedSteak extends JavaPlugin implements Listener {
 
@@ -30,6 +36,7 @@ public class EnchantedSteak extends JavaPlugin implements Listener {
 
         this.getLogger().info("Thank you for using the EnchantedSteak plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
 
+        checkForUpdates();
     }
 
     private void setupCraftingRecipe() {
@@ -66,6 +73,46 @@ public class EnchantedSteak extends JavaPlugin implements Listener {
             if (meta != null && meta.hasLore() && meta.getLore().contains(LORE_TEXT)) {
                 player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 3600, 0)); // 1200 ticks = 1 minute
             }
+        }
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
         }
     }
 }
